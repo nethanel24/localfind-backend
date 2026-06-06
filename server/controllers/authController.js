0c0447a00260e6d7c7e1714dd49d73151fd52c19
nethanel24 <nethanel24@gmail.com>
2026-06-06 19:33:42 +0300

    fix: add login to auth controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -22,3 +22,23 @@ exports.register = async (req, res, next) => {
     next(error);
   }
 };
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: 'Please enter email and password' });
+    }
+    const user = await User.findOne({ email }).select('+password');
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+    const isMatch = await user.matchPassword(password);
+    if (!isMatch) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+    const token = generateToken(user._id);
+    res.status(200).json({ success: true, token });
+  } catch (error) {
+    next(error);
+  }
+};
