0cfa16eee5c4f2d3a8d2fe07e5b55b52e11c8497
nethanel24 <nethanel24@gmail.com>
2026-06-06 18:41:02 +0300

    add auth controller with register

---
+++ server/controllers/authController.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const generateToken = (id) => {
+  return jwt.sign(
+    { id },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_EXPIRE || '30d' }
+  );
+};
+
+// @desc Register a new user
+// @route POST /api/auth/register
+// @access Public
+exports.register = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = await User.create({ name, email, password });
+    const token = generateToken(user._id);
+    res.status(201).json({ success: true, token });
+  } catch (error) {
+    next(error);
+  }
+};
