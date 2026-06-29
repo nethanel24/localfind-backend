3bb9229646dd377e056123c9be737ddf4e0b9520
nethanel24 <nethanel24@gmail.com>
2026-06-29 13:31:50 +0300

    feat: add JOI validation for review routes

--- server/routes/reviewRoutes.ts
+++ server/routes/reviewRoutes.ts
@@ -1,10 +1,10 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware";
 import { getReviewsByProvider, createReview } from "../controllers/reviewController";
-
+import validate from "../middleware/validate";
+import { createReviewSchema } from "../validation/reviewValidation";
 const router = express.Router();
-
-router.get("/", getReviewsByProvider);
+router.get("/", validate(createReviewSchema),getReviewsByProvider);
 router.post("/add", protect, createReview);
 
 export default router;
