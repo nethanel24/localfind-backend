ef4c8950b3d6e8f4fd1a684027c2fbe93de6001f
nethanel24 <nethanel24@gmail.com>
2026-07-16 19:10:14 +0300

    Save the role selected at registration instead of always defaulting to user

--- server/validation/authValidation.ts
+++ server/validation/authValidation.ts
@@ -4,6 +4,7 @@ export const registerSchema = Joi.object({
   name: Joi.string().min(2).max(50).required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
+  role: Joi.string().valid("user", "provider"),
 });
 
 export const loginSchema = Joi.object({
